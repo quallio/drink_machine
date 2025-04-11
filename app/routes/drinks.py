d5034fba69a11310e0f9726ded4778a306998581
quallio <facuq93@gmail.com>
2025-04-11 19:10:01 -0300

    get drinks update

--- app/routes/drinks.py
+++ app/routes/drinks.py
@@ -1,42 +1,36 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.future import select
 from sqlalchemy.sql import delete
 from app.database import get_db
 from app.models import Drink, DrinkIngredient
 from app.schemas import DrinkCreate, DrinkResponse
-from app.crud import get_drinks, create_drink
+from app.crud import create_drink, get_drinks_with_availability, prepare_drink_logic
 
-## PARA PRENDER LED ##
+# PARA PRENDER LED
 import RPi.GPIO as GPIO
 import threading
 import time
-#########################
+
 # GPIO 17, 27, 23 y 24 --> bombas 1, 2, 3 y 4
-#########################
-#########################
 PUMP_LED_PINS = {
     1: 17,
     2: 27,
     3: 23,
     4: 24
 }
-#######################################
+
 # Setup GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 for pin in PUMP_LED_PINS.values():
     GPIO.setup(pin, GPIO.OUT)
-#######################################
-
-
 
 router = APIRouter(prefix="/drinks", tags=["Drinks"])
 
-# Obtener lista de Drinks con sus ingredientes
+# ✅ Obtener lista de Drinks con info de disponibilidad
 @router.get("/", response_model=list[DrinkResponse])
 async def read_drinks(db: AsyncSession = Depends(get_db)):
-    return await get_drinks(db)
+    return await get_drinks_with_availability(db)
 
 # Crear un nuevo Drink con ingredientes
 @router.post("/", response_model=DrinkResponse)
@@ -50,22 +44,16 @@ async def remove_drink(drink_id: int, db: AsyncSession = Depends(get_db)):
     if not drink:
         raise HTTPException(status_code=404, detail="Drink not found")
 
-    # 🔹 Eliminar los registros en drink_ingredients sin tocar ingredients
     await db.execute(delete(DrinkIngredient).where(DrinkIngredient.drink_id == drink_id))
-
-    # 🔹 Eliminar el Drink
     await db.delete(drink)
     await db.commit()
 
     return {"message": "Drink deleted"}
 
-from app.crud import prepare_drink_logic  # Asegurate de importar esto arriba
-
 # Preparar un drink
 @router.post("/prepare/{drink_id}")
 async def prepare_drink(drink_id: int, db: AsyncSession = Depends(get_db)):
     return await prepare_drink_logic(db, drink_id)
-
 
 # Testea las salidas (los 4 leds)
 @router.post("/test-leds/{tiempo}")
